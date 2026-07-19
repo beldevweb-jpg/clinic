91abfa466b02e4068b51d996568e95efcc58e706
Bel <bel.dev.web@gmail.com>
2026-07-19 23:35:31 +0700

    Update head.blade.php

--- Modules/Core/resources/views/components/layouts/head.blade.php
+++ Modules/Core/resources/views/components/layouts/head.blade.php
@@ -1,9 +1,23 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
+<title>คลินิกการแพทย์แผนไทย</title>
+
+
+<!-- Bootstrap -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
+
+<!-- Bootstrap Icon -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+
+
+<link href="https://cdn.jsdelivr.net/npm/tom-select/dist/css/tom-select.css" rel="stylesheet">
+
+
+<link rel="preconnect" href="https://fonts.bunny.net">
+<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet">
 
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
